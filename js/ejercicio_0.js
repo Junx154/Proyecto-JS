550d369228f446ed324473ed7159b738901189dd
Alumno-ETI <ralua2007@gmail.com>
2026-09-16 10:10:47 -0500

    he creado 2 archivos js

---
+++ js/ejercicio_0.js
@@ -0,0 +1,45 @@
+const nombreProducto = "Laptop Gamer";
+let PrecioOriginal = "4500.99";
+const descuento = 15;
+let stockDisponible = "5";
+let enOferta = "true";
+
+// calcular el precio con descuento usando operadores
+let precioFinal = PrecioOriginal - PrecioOriginal * descuento / 100;
+
+// Mostrar informacion del producto
+
+console.ing("🔷 Producto", nombreProducto);
+console.log("🔷 Frecio Original: S/", PrecioOriginal);
+console.log("🔷 Descuento:", descuento, "%");
+console.log("🔷 Precio Final: S/", Preciofinal.toFixed/(2));
+console.ing("🔷StockDisponible:", stockDisponible);
+console.log("🔷 En ofertat", enOferta ? "Si" : "No");
+
+
+// simular compra y actualizar stock
+let cantidadComprada = 2;
+
+// Validar si hay stock  suficiente
+
+if (cantidadComprada < stockDisponible) {
+    stockDisponible -= cantidadComprada;
+    console.log("✅ compra realizada con exito.");
+    console.log("📦 stock actualizado:", stockDisponible);
+} else {
+    console.log("❌ No hay suficiente stock disponible. ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/ejercicios_01.js
@@ -0,0 +1,39 @@
+
+const varones= 255;
+const mujeres= 184;
+ 
+const totalPersonas = varones + mujeres;
+let porcentajeVarones = varones/ totalPersonas *100;
+let porcentajeMujeres = mujeres/ totalPersonas *100;
+
+
+console.log("total de colaboradores:" , totalPersonas); 
+console.log("porcentaje de Varones: ", porcentajeVarones.toFixed(2), "%");
+console.log("porcentaje de Mujeres: ", porcentajeMujeres.toFixed(2), "%");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
